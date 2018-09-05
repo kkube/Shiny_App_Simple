241c8c6bbaa5e839f329838a8c0efcd96d5bb094
kkube <kyle.kubeczka17@gmail.com>
2018-09-05 00:16:43 -0500

    Make app reactive to user input for title, insert graph of gdpPercap vs lifeExp

--- ShinyApp.R
+++ ShinyApp.R
@@ -1,7 +1,27 @@
 library(shiny)
+library(gapminder)
+library(ggplot2)
 
-ui <- fluidPage()
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      textInput(inputId = "title", 
+                label = "Title", 
+                "GDP vs life exp")
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    ggplot(gapminder, aes(gdpPercap, lifeExp)) +
+      geom_point() +
+      scale_x_log10() +
+      ggtitle(input$title)
+  })
+}
 
 shinyApp(ui = ui, server = server)
